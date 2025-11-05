8354e99a8c1541d979e45b360da0ca64be4da238
Serda Özün <serda@mailbox.org>
2025-11-05 19:33:03 +0100

    update ktor to 3.3.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ val flyway_version: String by project
 
 plugins {
     kotlin("jvm") version "2.2.20"
-    id("io.ktor.plugin") version "3.3.0"
+    id("io.ktor.plugin") version "3.3.2"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.20"
     id("com.github.node-gradle.node") version "7.1.0"
 }
